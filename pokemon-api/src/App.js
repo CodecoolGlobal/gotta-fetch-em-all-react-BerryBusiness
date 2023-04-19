cb68b52ce6e4e51c377a985f059c0a279fec76ee
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2023-04-19 05:51:48 +0200

    all the fetches

--- pokemon-api/src/App.js
+++ pokemon-api/src/App.js
@@ -9,45 +9,55 @@ function App() {
 const [locations, setLocations] = useState([]);
 const [clickedLocation, setClickedLocation] = useState(false);
 const [pokemons, setPokemons] = useState([]);
-const [selectedArea, setSeledtedArea] = useState(1);
+const [areas, setAreas] = useState([]);
+const [onLocation, setOnLocation] = useState([]);
 
-console.log('selectedA:', selectedArea);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/location")
+    fetch("https://pokeapi.co/api/v2/location") //fetch all locations
       .then((res) => res.json())
       .then((data) => setLocations(data.results));
     }, []);
    
+  useEffect(() => {
+    fetch(`${onLocation.url}`)  //fetch areas of clicked loaction
+      .then((res) => res.json())
+      .then((data) => setAreas(data.areas));
+    }, [onLocation]);
+
+    
+    const getRandomArea  = Math.floor(Math.random() * areas.length);
+    console.log('randomArea', getRandomArea);
+    
+    console.log('areas: ', areas[getRandomArea]);
+
     useEffect(() => {
-      fetch(`https://pokeapi.co/api/v2/location-area/${selectedArea}/`) //adott loaction encounterei kellenek - ferchben fetch?
+      fetch(`https://pokeapi.co/api/v2/location-area/1/`)   //fetch encounters from random chosen area
         .then((res) => res.json())
         .then((data) => setPokemons(data.pokemon_encounters));
-      }, [selectedArea]);
+      }, []);
 
-  const getRandomPoke  = Math.floor(Math.random() * pokemons.length) + 1;
-      console.log(getRandomPoke);
+  const getRandomPoke  = Math.floor(Math.random() * pokemons.length);
+      console.log('randomPoke', getRandomPoke);
+ 
+  const chosenPokemon = pokemons[getRandomPoke]
+  console.log(chosenPokemon.pokemon.url);
 
-  
-  /* const handlerLocation = (location) {
-    selectedArea(location)
-  }  */ 
-  const handleLocation = (event) => {
-    event.preventDefault();
-    setSeledtedArea(event + 1);
-    setClickedLocation(true);
+   /*  console.log('loc:', locations);
+    console.log('pokes:', pokemons);
+ */
+  const onLocationSelected = (location) => {
+      setOnLocation(location)
   }
 
-    console.log('loc:', locations);
-    console.log('pokes:', pokemons);
-  
+  console.log('onLocation', onLocation);
 
   return (
     <div className="App">
       { clickedLocation ? (
         <div>
           {
-            <Pokemons pokemons={pokemons[getRandomPoke]} 
+            <Pokemons pokemons={chosenPokemon} 
                      
             />
             
@@ -60,7 +70,7 @@ console.log('selectedA:', selectedArea);
             
             <Locations locations={locations} 
             setClickedLocation={setClickedLocation}
-            onLocationSelected={(location) => console.log(location)}
+            onLocationSelected={onLocationSelected}
             />
           }
         </div>
